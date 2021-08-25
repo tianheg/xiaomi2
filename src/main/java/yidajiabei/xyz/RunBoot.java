ce8d334ff89a9a38597c666a4987ccc1612ee370
tianheg <yidajiabei@163.com>
2021-08-25 14:08:19 +0800

    initial commit

---
+++ src/main/java/yidajiabei/xyz/RunBoot.java
@@ -0,0 +1,11 @@
+package yidajiabei.xyz;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//@MapperScan(basePackages = "yidajiabei.xyz.mapper");
+
+@SpringBootApplication
+public class RunBoot {
+    public static void main(String[] args) {
+        SpringApplication.run(RunBoot.class,args);
+    }
+}
