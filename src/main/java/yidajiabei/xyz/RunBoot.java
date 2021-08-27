9024a8dd45ba5f42116506ca5528feb3b6d08adb
tianheg <yidajiabei@163.com>
2021-08-27 08:41:53 +0800

    update: RunBoot

--- src/main/java/yidajiabei/xyz/RunBoot.java
+++ src/main/java/yidajiabei/xyz/RunBoot.java
@@ -1,10 +1,10 @@
-package yidajiabei.xyz;
+package yidajiabei.xyz; // alt + enter move class to certain position
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication // annotation
 public class RunBoot {
-    public static void main(String[] args) {
+    public static void main(String[] args) { // main class
         SpringApplication.run(RunBoot.class,args);
     }
 }
